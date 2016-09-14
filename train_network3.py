515b67a4d0dfe080089eff1e3a7510e57e449e7e
alexey-ernest <alexey.ernest@gmail.com>
2016-09-14 20:46:18 +0300

    Extra fully connected layer

--- train_network3.py
+++ train_network3.py
@@ -78,8 +78,28 @@ mini_batch_size = 10
 
 
 # Expanded training data: 50,000 x 5 = 250K
-# ~
-expanded_training_data, _, _ = network3.load_data_shared("./data/mnist_expanded.pkl.gz")
+# ~99.16% (2 hours)
+# expanded_training_data, _, _ = network3.load_data_shared("./data/mnist_expanded.pkl.gz")
+# net = Network([
+#             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+#                           filter_shape=(20, 1, 5, 5),
+#                           poolsize=(2, 2),
+#                           activation_fn=ReLU),
+#             ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+#                           filter_shape=(40, 20, 5, 5),
+#                           poolsize=(2, 2),
+#                           activation_fn=ReLU),
+#             FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
+#             SoftmaxLayer(n_in=100, n_out=10)
+#         ],
+#         mini_batch_size)
+# net.SGD(training_data, 60, mini_batch_size, 0.03,
+#         validation_data, test_data, lmbda=0.1)
+
+
+# Extra fully connected layer
+#
+#expanded_training_data, _, _ = network3.load_data_shared("./data/mnist_expanded.pkl.gz")
 net = Network([
             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                           filter_shape=(20, 1, 5, 5),
@@ -90,6 +110,7 @@ net = Network([
                           poolsize=(2, 2),
                           activation_fn=ReLU),
             FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
+            FullyConnectedLayer(n_in=100, n_out=100, activation_fn=ReLU),
             SoftmaxLayer(n_in=100, n_out=10)
         ],
         mini_batch_size)
