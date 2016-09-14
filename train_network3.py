ce39c5816d3c4a45b1bf4679599f71b5d62e17d6
alexey-ernest <alexey.ernest@gmail.com>
2016-09-14 16:28:09 +0300

    Rectified linear units & regularization

--- train_network3.py
+++ train_network3.py
@@ -3,6 +3,7 @@ from timeit import default_timer as timer
 import network3
 from network3 import Network
 from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+from network3 import ReLU
 
 # load data
 training_data, validation_data, test_data = network3.load_data_shared()
@@ -41,19 +42,39 @@ mini_batch_size = 10
 
 
 # Two conv layers with 20 feature maps and 40 feature maps
-# ~98.99% epcho 43
+# ~98.99% epoch 43
+# net = Network([
+#             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+#                           filter_shape=(20, 1, 5, 5),
+#                           poolsize=(2, 2)),
+#             ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+#                           filter_shape=(40, 20, 5, 5),
+#                           poolsize=(2, 2)),
+#             FullyConnectedLayer(n_in=40*4*4, n_out=100),
+#             SoftmaxLayer(n_in=100, n_out=10)
+#         ],
+#         mini_batch_size)
+# net.SGD(training_data, 60, mini_batch_size, 0.1,
+#         validation_data, test_data)
+
+
+# Rectified linear units activation & regularization
+# ~
 net = Network([
             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                           filter_shape=(20, 1, 5, 5),
-                          poolsize=(2, 2)),
+                          poolsize=(2, 2),
+                          activation_fn=ReLU),
             ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
                           filter_shape=(40, 20, 5, 5),
-                          poolsize=(2, 2)),
-            FullyConnectedLayer(n_in=40*4*4, n_out=100),
+                          poolsize=(2, 2),
+                          activation_fn=ReLU),
+            FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
             SoftmaxLayer(n_in=100, n_out=10)
         ],
         mini_batch_size)
-net.SGD(training_data, 60, mini_batch_size, 0.1,
-        validation_data, test_data)
+net.SGD(training_data, 60, mini_batch_size, 0.03,
+        validation_data, test_data, lmbda=0.1)
+
 
 print('Learning time {0}'.format(timer() - start))
