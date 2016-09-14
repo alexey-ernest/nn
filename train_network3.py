18e8d6fe5c458eb612f919ab83a024af4dedaf5b
alexey-ernest <alexey.ernest@gmail.com>
2016-09-14 13:27:52 +0300

    Two conv layers net

--- train_network3.py
+++ train_network3.py
@@ -28,16 +28,32 @@ mini_batch_size = 10
 
 # One conv layer with 20 feature maps
 # ~98.78% (98.49% without fully-connected layer)
+# net = Network([
+#             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+#                           filter_shape=(20, 1, 5, 5),
+#                           poolsize=(2, 2)),
+#             FullyConnectedLayer(n_in=20*12*12, n_out=100),
+#             SoftmaxLayer(n_in=100, n_out=10)
+#         ],
+#         mini_batch_size)
+# net.SGD(training_data, 60, mini_batch_size, 0.1,
+#         validation_data, test_data)
+
+
+# Two conv layers with 20 feature maps and 40 feature maps
+# ~99%
 net = Network([
             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                           filter_shape=(20, 1, 5, 5),
                           poolsize=(2, 2)),
-            FullyConnectedLayer(n_in=20*12*12, n_out=100),
+            ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                          filter_shape=(40, 20, 5, 5),
+                          poolsize=(2, 2)),
+            FullyConnectedLayer(n_in=40*4*4, n_out=100),
             SoftmaxLayer(n_in=100, n_out=10)
         ],
         mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.1,
         validation_data, test_data)
-
 
 print('Learning time {0}'.format(timeit.timeit() - start))
