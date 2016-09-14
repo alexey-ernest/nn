4562b886e14410ca90fa0129898f7dc75bb55f03
alexey-ernest <alexey.ernest@gmail.com>
2016-09-14 19:10:39 +0300

    Expanded training set

--- train_network3.py
+++ train_network3.py
@@ -59,7 +59,27 @@ mini_batch_size = 10
 
 
 # Rectified linear units activation & regularization
+# ~99.19%
+# net = Network([
+#             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+#                           filter_shape=(20, 1, 5, 5),
+#                           poolsize=(2, 2),
+#                           activation_fn=ReLU),
+#             ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+#                           filter_shape=(40, 20, 5, 5),
+#                           poolsize=(2, 2),
+#                           activation_fn=ReLU),
+#             FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
+#             SoftmaxLayer(n_in=100, n_out=10)
+#         ],
+#         mini_batch_size)
+# net.SGD(training_data, 60, mini_batch_size, 0.03,
+#         validation_data, test_data, lmbda=0.1)
+
+
+# Expanded training data: 50,000 x 5 = 250K
 # ~
+expanded_training_data, _, _ = network3.load_data_shared("./data/mnist_expanded.pkl.gz")
 net = Network([
             ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                           filter_shape=(20, 1, 5, 5),
