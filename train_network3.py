7fd8f69e38ef94962edeb6915a1588f3ff2a22c5
alexey-ernest <alexey.ernest@gmail.com>
2016-09-14 13:20:35 +0300

    Convnet example

---
+++ train_network3.py
@@ -0,0 +1,43 @@
+import timeit
+
+import network3
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+
+# load data
+training_data, validation_data, test_data = network3.load_data_shared()
+
+# train
+start = timeit.timeit()
+mini_batch_size = 10
+
+# Shallow network with 1 hidden layer of 100 sigmoid neurons, 60 epochs,
+# 10 mini batch size and 0.1 learning rate, no regularization,
+# softmax final layer with log-likelihood cost function.
+# ~97.78%
+# net = Network([
+#         FullyConnectedLayer(n_in=784, n_out=100),
+#         SoftmaxLayer(n_in=100, n_out=10)
+#     ],
+#     mini_batch_size)
+
+# net.SGD(training_data, 60, mini_batch_size, 0.1,
+#         validation_data, test_data)
+
+
+
+# One conv layer with 20 feature maps
+# ~98.78% (98.49% without fully-connected layer)
+net = Network([
+            ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                          filter_shape=(20, 1, 5, 5),
+                          poolsize=(2, 2)),
+            FullyConnectedLayer(n_in=20*12*12, n_out=100),
+            SoftmaxLayer(n_in=100, n_out=10)
+        ],
+        mini_batch_size)
+net.SGD(training_data, 60, mini_batch_size, 0.1,
+        validation_data, test_data)
+
+
+print('Learning time {0}'.format(timeit.timeit() - start))
