d84a4c6d2b932b5035641cc2f6da38b52e1c33c5
alexey-ernest <alexey.ernest@gmail.com>
2016-10-04 13:18:41 +0300

    Tensorflow conv mnist

--- tensorflow/mnist.py
+++ tensorflow/mnist.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
 
 # define symbolic var for input data: None means that a dimension can be of any length
